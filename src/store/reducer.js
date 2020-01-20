6c07f77bda3da5bfffd0749310435c815955afcb
weblw <872993425@qq.com>
2020-01-20 17:54:01 +0800

    完成 axios 数据请求

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux-immutable'
+import { reducer as recommendReducer } from '../application/Recommend/store/index'
 
 export default combineReducers({
   // 添加具体的reducer
+  recommend: recommendReducer
 })
